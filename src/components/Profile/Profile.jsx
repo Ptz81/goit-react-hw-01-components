8c110d2daa06a4be40d4d8de511c0e9bb6db1202
Taras Papka <taras.papka@protonmail.com>
2023-04-28 23:04:30 +0300

    added styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 
 export const Profile = ({username, tag, location, avatar, stats}) => {
   return (
-<div class="profile">
+<div className={css.profile}>
   <div className={css.description}>
     <img
       src={avatar}
@@ -13,22 +13,23 @@ export const Profile = ({username, tag, location, avatar, stats}) => {
       className={css.avatar}
     />
     <p className={css.name}>{username}</p>
-    <p className={css.tag}>{tag}</p>
+    <p className={css.tag}>@{tag}</p>
     <p className={css.location}>{location}</p>
   </div>
 
-  <ul className={css.stats}>
+      <ul className={css.stats}>
+
     <li>
-      <span className="followers">Followers</span>
-      <span className="quantity">{stats.followers}</span>
+      <span className={css.label}>Followers:</span>
+      <span className={css.quantity}>{stats.followers}</span>
     </li>
       <li>
-      <span className="label">Views</span>
-      <span className="quantity">{stats.views}</span>
+      <span className={css.label}>Views:</span>
+      <span className={css.quantity}>{stats.views}</span>
     </li>
       <li>
-      <span className="label">Likes</span>
-      <span className="quantity">{stats.likes}</span>
+      <span className={css.label}>Likes:</span>
+      <span className={css.quantity}>{stats.likes}</span>
     </li>
   </ul>
 </div>
