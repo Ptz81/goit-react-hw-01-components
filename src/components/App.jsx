28a682fa98637f1f111162bf71e67c891397f4e5
Taras Papka <taras.papka@protonmail.com>
2023-04-29 22:32:14 +0300

    finished style for 2 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,19 @@ import data from './Statistics/data.json'
 
 export const App = () => {
   return (
+    <>
     <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}
-    />,
- <Statistics title="Upload stats" stats={data} />
+    />
+    <Statistics title="Upload stats" stats={data} />
+    {/* <FriendList friends={friends} />
+    <TransactionHistory items={transactions} /> */}
+
+    </>
+
   );
 };
