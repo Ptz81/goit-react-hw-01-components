bdc7e94f0365c45a70d8b195077a9cfd72533776
Taras Papka <taras.papka@protonmail.com>
2023-04-30 00:19:01 +0300

    task 3

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -4,9 +4,9 @@ import css from './FriendList.module.css'
 export function FriendListItem({ status, avatar, name }) {
   return (
     <li className={css.item}>
-      <span className={css.status? css.online : css.offline}>{status}</span>
+      <span className={status? css.online : css.offline}></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-      <p className={css.name}>{name}</p>
+      <p className={status? css.name : css.noname}>{name}</p>
 </li>
   )
 }
