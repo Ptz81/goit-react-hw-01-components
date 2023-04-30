a7ef0c236de3cf3c1d83f13fc4dc7e8558a10298
Taras Papka <taras.papka@protonmail.com>
2023-04-30 20:00:47 +0300

    added the default img

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import css from './FriendList.module.css'
-import {FriendListItem} from "./FriendListItem"
+import { FriendListItem } from "./FriendListItem"
 import PropTypes from 'prop-types';
 
 export function FriendList({ friends }) {
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,11 +1,14 @@
-
+import defaultimg from './default.png'
 import css from './FriendList.module.css'
 
 export function FriendListItem({ status, avatar, name }) {
+  if (!avatar) {
+    avatar = defaultimg;
+  }
   return (
     <li className={css.item}>
       <span className={status? css.online : css.offline}></span>
-      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <img className={css.avatar} src={avatar??defaultimg} alt="User avatar" width="48" />
       <p className={status? css.name : css.noname}>{name}</p>
 </li>
   )
