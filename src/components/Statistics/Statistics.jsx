f9a9438e99f965fea4c73552e85e9c72877a4a4b
Taras Papka <taras.papka@protonmail.com>
2023-04-30 22:34:18 +0300

    made correction based on mentors advice

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,7 +9,7 @@ export const Statistics = ({title, stats}) => {
 
   return (
     <section className={css.statistics}>
-  <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => (
